f6a6dab2d7158266fac2927a58c1b4a0744bd973
patrik909 <patrik.eriksson@medieinstitutet.se>
2019-02-24 16:59:25 +0100

    filtrering front end jobs with array filter

--- main.js
+++ main.js
@@ -2,34 +2,42 @@
 // http://apikatalogen.se/api/carrerjet-api
 // http://apikatalogen.se/api/appjobb-api
 const jobListOutput = document.getElementById('job-list-output')
+const frontEndJobsButton = document.getElementById('front-end-jobs-button')
+const jobAmountOutput = document.getElementById('job-amount-output')
 const AMS_BASEURL = 'http://api.arbetsformedlingen.se/af/v0/platsannonser/'
 // soklista/yrken?yrkesgruppid=2512
 
-
+allJobsArray = []
 
 fetchJobs = (searchURL) => {
   return (
     fetch(AMS_BASEURL + searchURL)
     .then((res) => res.json())
     .then((jobs) => {
+      allJobsArray = jobs
       return jobs
     })
   )
 }
 
 listJobs = (jobs) => {
-  console.log(jobs)
   listedJobs = ''
-  jobs.matchningslista.matchningdata.map(job => {
+  jobs.map(job => {
     listedJobs += `
       <li>
         <p>${job.annonsrubrik}</p>
         <p>${job.arbetsplatsnamn}</p>
       </li>
-    `;
+    `
   })
+  jobAmountOutput.innerHTML = jobs.length
   jobListOutput.innerHTML = listedJobs
 }
 
 // Fetch and list front end developer jobs
-fetchJobs('matchning?yrkesid=7633&antalrader=10000').then(resp => listJobs(resp))
+fetchJobs('matchning?yrkesid=7633&antalrader=10000').then(resp => listJobs(resp.matchningslista.matchningdata))
+
+frontEndJobsButton.addEventListener('click', (event) => {
+  let frontEndJobs = allJobsArray.matchningslista.matchningdata.filter(job => job.yrkesbenamning === 'Frontend-utvecklare')
+  listJobs(frontEndJobs)
+})
