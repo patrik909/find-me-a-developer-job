8995c6bec8d23655f4fd27c56a3736d7fe7b8122
patrik909 <patrik.eriksson@medieinstitutet.se>
2019-02-24 20:53:07 +0100

    array filter on front-end jobs in stockholm

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 // http://apikatalogen.se/api/arbetsformedlingen-api
+const AMS_BASEURL = 'http://api.arbetsformedlingen.se/af/v0/platsannonser/'
 // http://apikatalogen.se/api/carrerjet-api
 // http://apikatalogen.se/api/appjobb-api
+
 const jobListOutput = document.getElementById('job-list-output')
+const allJobsButton = document.getElementById('all-jobs-button')
 const frontEndJobsButton = document.getElementById('front-end-jobs-button')
+const stockholmLocationButton = document.getElementById('stockholm-location-button')
 const jobAmountOutput = document.getElementById('job-amount-output')
-const AMS_BASEURL = 'http://api.arbetsformedlingen.se/af/v0/platsannonser/'
 // soklista/yrken?yrkesgruppid=2512
+//matchning?nyckelord="bagare"
+
+// Användbarhetsdesigner 6528
+// Applikationsutvecklare/Apputvecklare 7576
+// Interaktionsdesigner 7575
+// Systemutvecklare/Programmerare 2419
+
+fetch(AMS_BASEURL + 'soklista/yrken?yrkesgruppid=2512')
+.then((res) => res.json())
+.then((jobs) => {
+  console.log(jobs)
+})
 
 allJobsArray = []
 
@@ -27,17 +42,34 @@ listJobs = (jobs) => {
       <li>
         <p>${job.annonsrubrik}</p>
         <p>${job.arbetsplatsnamn}</p>
+        <p>${job.anstallningstyp}</p>
+        <p>${job.kommunnamn}</p>
+        <p>${job.publiceraddatum}</p>
+        <p>${job.sista_ansokningsdag}</p>
+        <p>${job.yrkesbenamning}</p>
       </li>
     `
+    // console.log(job)
   })
   jobAmountOutput.innerHTML = jobs.length
   jobListOutput.innerHTML = listedJobs
 }
 
 // Fetch and list front end developer jobs
-fetchJobs('matchning?yrkesid=7633&antalrader=10000').then(resp => listJobs(resp.matchningslista.matchningdata))
+fetchJobs('matchning?yrkesid=7633&antalrader=10000')
+.then(resp => listJobs(resp.matchningslista.matchningdata))
+
+allJobsButton.addEventListener('click', () => {
+  listJobs(allJobsArray.matchningslista.matchningdata)
+})
 
 frontEndJobsButton.addEventListener('click', (event) => {
   let frontEndJobs = allJobsArray.matchningslista.matchningdata.filter(job => job.yrkesbenamning === 'Frontend-utvecklare')
   listJobs(frontEndJobs)
+})
+
+stockholmLocationButton.addEventListener('click', (event) => {
+  let stockholmJobs = allJobsArray.matchningslista.matchningdata.filter(job => job.kommunnamn === 'Stockholm')
+  let filtredStockholmJobs = stockholmJobs.filter(job => job.yrkesbenamning === 'Frontend-utvecklare')
+  listJobs(filtredStockholmJobs)
 })
